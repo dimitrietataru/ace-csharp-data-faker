075400594d752393a299c4a3b489824d0b02d042
Dimitrie Tataru <dimitriettr@gmail.com>
2023-01-23 00:13:34 +0200

    Add resources

---
+++ src/Ace.CSharp.DataFaker/Internal/Resources/DataFakerOptionsResources.cs
@@ -0,0 +1,11 @@
+namespace Ace.CSharp.DataFaker.Internal.Resources;
+
+internal static class DataFakerOptionsResources
+{
+    public const string DefaultLocaleCode = "en_US";
+    public const int DefaultCollectionCount = 3;
+
+    public const string DefaultPrefix = "Fake";
+    public const string DefaultGenericPrefix = "Of";
+    public const string DefaultGenericSeparator = "And";
+}
