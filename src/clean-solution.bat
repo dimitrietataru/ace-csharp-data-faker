dd7bffabd66694092f5cdad59740c7961806c60a
MYT <dimitriettr@gmail.com>
2023-06-05 13:38:59 +0300

    Add 'clean-solution' script

---
+++ src/clean-solution.bat
@@ -0,0 +1,10 @@
+rmdir /s /q .\Ace.CSharp.DataFaker\bin
+rmdir /s /q .\Ace.CSharp.DataFaker\obj
+
+rmdir /s /q .\Ace.CSharp.DataFaker.Sample\bin
+rmdir /s /q .\Ace.CSharp.DataFaker.Sample\obj
+
+rmdir /s /q .\Ace.CSharp.DataFaker.Tests\bin
+rmdir /s /q .\Ace.CSharp.DataFaker.Tests\obj
+
+pause
